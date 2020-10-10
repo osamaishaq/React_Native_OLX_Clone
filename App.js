4be749fffa1da7b55067f9c7013bb94ca4e3bb99
Osama Ishaq <39963874+osamaishaq@users.noreply.github.com>
2020-10-11 00:06:46 +0500

    Uploaded version 1.0 of OLX_Clone

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppLoading } from "expo";
+
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+import OfflinmeNotice from "./app/components/OfflinmeNotice";
+import AuthContext from "./app/auth/authContext";
+import AuthStorage from "./app/auth/storage";
+
+export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreToken = async () => {
+    const user = await AuthStorage.getUser();
+    if (!user) return;
+
+    setUser(user);
+  };
+
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+    );
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <NavigationContainer theme={navigationTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
